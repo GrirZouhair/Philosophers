1121b8d4138990b922b5e863ba60a9347a2693eb
GRIRZouhair <zouhairgrir937@gmail.com>
2025-04-11 03:26:09 +0100

    basic implementation of the lifesycle of philosophers

--- MANDATORY/lifesycle.c
+++ MANDATORY/lifesycle.c
@@ -6,7 +6,7 @@
 /*   By: zogrir <zogrir@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 22:36:28 by zogrir            #+#    #+#             */
-/*   Updated: 2025/04/11 02:48:58 by zogrir           ###   ########.fr       */
+/*   Updated: 2025/04/11 03:24:46 by zogrir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,27 +30,36 @@ void ft_message(t_philo *philo, char *mesg)
 {
     pthread_mutex_lock(philo->write_lock);
     if (!philo_dead_check(philo))
-        printf("%lld %d %s", get_time() - philo->start_time, philo->id, mesg);
+        printf("%lld %d %s\n", get_time() - philo->start_time, philo->id, mesg);
     pthread_mutex_unlock(philo->write_lock);
 }
 
 
-// void* ft_lifesycle(void *arg)
-// {
-// 	// t_philo *philo;
-    
-//     // philo = (t_philo *)arg;
-//     //  printf("yup1");
-//     // if (philo->data->num_philos == 1)
-//     // {
-//     //     ft_message(philo, "has taken a fork");
-//     //     return(0);
-//     // }
-//     // return(NULL);
-//     (void )arg;
-//     write(1, "abc\n", 4);
-//     return(NULL);
-// }
+void	dream(t_philo *philo)
+{
+	ft_message(philo, "Is Sleeping");
+	usleep_precise(100);
+	ft_message(philo, "Is Thinking");
+}
+
+void eat(t_philo *philo)
+{
+	pthread_mutex_lock(philo->r_fork);
+	ft_message(philo, "Has Take A Fork");
+	pthread_mutex_lock(philo->l_fork);
+	ft_message(philo, "Has Taken A Fork");
+	ft_message(philo, "Is Eating");
+	pthread_mutex_lock(philo->meal_lock);
+	philo->eating_flag = 1;
+	philo->last_meal = get_time();
+	philo->meals_eaten++;
+	philo->eating_flag = 0;
+	pthread_mutex_unlock(philo->meal_lock);
+	usleep_precise(100);
+	pthread_mutex_unlock(philo->l_fork);
+	pthread_mutex_unlock(philo->r_fork);
+}
+
 
 
 void* ft_lifesycle(void *arg)
@@ -66,7 +75,13 @@ void* ft_lifesycle(void *arg)
             usleep_precise(100);
             return(0);
         }
-        
     }
-    return (NULL);
+	if (philo->id % 2 == 0)
+		usleep_precise(100);
+	while (!philo_dead_check(philo))
+	{
+		eat(philo);
+		dream(philo);
+	}
+    return (0);
 }
